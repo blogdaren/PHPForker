4c4c90e0c4dcb9c4e24a512cd34740cdb7443dc7
blogdaren <blogdaren@163.com>
2019-01-25 16:12:45 +0800

    add Connection Class to support reboot|stop|reload gracefully

---
+++ Core/Connection.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @script   Connection.php
+ * @brief    This file is part of PHPForker
+ * @author   blogdaren<blogdaren@163.com>
+ * @version  1.0.1
+ * @modify   2019-01-25
+ */
+
+namespace PHPForker\Core;
+use PHPForker\Container;
+
+class Connection 
+{
+    /**
+     * connection id
+     *
+     * @var array
+     */
+    public $id = 0;
+
+    /**
+     * id recorder
+     *
+     * @var int
+     */
+    static protected $_idRecorder = 1;
+
+    /**
+     * keep how many client connections for one process
+     *
+     * @var int
+     */
+    static public $connectionCount = 0;
+
+    /**
+     *  all connection instances
+     *
+     * @var array
+     */
+    static public $connections = array(); 
+
+
+    /**
+     * construct
+     *
+     * @param resource $socket
+     * @param string   $remote_address
+     */
+    public function __construct()
+    {
+        self::$connectionCount++;
+        $this->id = self::$_idRecorder++;
+        self::$_idRecorder === PHP_INT_MAX && self::$_idRecorder = 0;
+        self::$connections[$this->id] = $this;
+    }
+
+
+    /**
+     * destruct
+     *
+     * @return void
+     */
+    public function __destruct()
+    {
+        self::$connectionCount--;
+
+        if(Container::isGracefulStop() && self::$connectionCount <= 1) 
+        {
+            Container::stopAll();
+        }
+    }
+}
+
